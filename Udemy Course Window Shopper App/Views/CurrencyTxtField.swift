6b752ed4df1a328521409d14c3407b38faa52ca0
JeffTheShoe <cadenkowalski1@gmail.com>
2019-04-02 17:25:27 -0400

    Added basic UI elements

---
+++ Udemy Course Window Shopper App/Views/CurrencyTxtField.swift
@@ -0,0 +1,21 @@
+//
+//  CurrencyTxtField.swift
+//  Udemy Course Window Shopper App
+//
+//  Created by Caden Kowalski on 4/2/19.
+//  Copyright © 2019 Caden Kowalski. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
